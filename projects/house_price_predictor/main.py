ef2d5424de086a46d6209acd37a0c1acd3eb8308
Mark <mark.mazur007@gmail.com>
2024-12-17 00:36:56 -0500

    add plots.py and fix trainer.py

--- projects/house_price_predictor/main.py
+++ projects/house_price_predictor/main.py
@@ -1,13 +1,16 @@
 from trainer import Trainer
 
 def main():
-    trainer = Trainer(num_epochs=25, learning_rate=0.0001)
+    trainer = Trainer(num_epochs=100, learning_rate=0.0001)
 
     # Train the model
     trainer.train_model()
 
     # Evaluate the model
     trainer.evaluate_model()
+
+    # Plots
+    trainer.plot()
 
 if __name__ == '__main__':
     main()
--- projects/house_price_predictor/trainer.py
+++ projects/house_price_predictor/trainer.py
@@ -1,11 +1,13 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import numpy as np
 from torch.utils.data import TensorDataset, DataLoader, random_split
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 from typing import Tuple
 from preprocess_data import preprocess_data
 from network import HousePricePredictor
+from plots import plot_loss, plot_predicted_vs_actual
 
 
 class Trainer:
@@ -68,33 +70,52 @@ class Trainer:
             for features, labels in self.train_loader:
                 features, labels = features.to(self.device), labels.to(self.device)
 
+                # Forward pass
                 outputs = self.model(features)
-
                 loss = self.criterion(outputs, labels)
 
+                # Backpropogation
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
 
-                epoch_loss += loss.item()
+                epoch_loss += loss.item() # Add sum all of losses during one epoch
 
-            print(f'Epoch: [{epoch+1}/{self.num_epochs}], Loss: {epoch_loss/len(self.train_loader)}')
+            mean_loss = epoch_loss/len(self.train_loader) # Calculate mean loss in each epoch
+
+            print(f'Epoch: [{epoch+1}/{self.num_epochs}], Loss: {mean_loss}')
+
+            self.mean_losses.append(mean_loss) # Add callculated mean loss
 
     def evaluate_model(self) -> None:
         self.model.eval()
 
         mse, r2, mae = 0.0, 0.0, 0.0
+        self.eval_outputs, self.eval_labels = [], []
 
         with torch.no_grad():
-            for data, targets in self.test_loader:
-                outputs = self.model(data)
+            for features, labels in self.test_loader:
+                outputs = self.model(features)
 
-                targets, outputs = targets.cpu().numpy(), outputs.cpu().numpy()
+                labels, outputs = labels.cpu().numpy(), outputs.cpu().numpy() # Transform into cpu and numpy array
 
-                mse += mean_squared_error(targets, outputs)
-                r2 += r2_score(targets, outputs)
-                mae += mean_absolute_error(targets, outputs)
+                self.eval_outputs.append(outputs)
+                self.eval_labels.append(labels)
 
+                # Sum of all each metric
+                mse += mean_squared_error(labels, outputs)
+                r2 += r2_score(labels, outputs)
+                mae += mean_absolute_error(labels, outputs)
+
+        # Calculate mean and print metrics
         print(f"Mean Squared Error on the test loader: {mse/len(self.test_loader):.4f}")
         print(f"R2 Score on the test loader: {r2/len(self.test_loader):.4f}")
         print(f"Mean Absolute Error on the test loader: {mae/len(self.test_loader):.4f}")
+
+        # Concatenate all outputs and labels of evaluation together
+        self.eval_outputs, self.eval_labels = np.concat(self.eval_outputs), np.concat(self.eval_labels)
+
+    def plot(self) -> None:
+        plot_loss(self.mean_losses)
+
+        plot_predicted_vs_actual(self.eval_outputs, self.eval_labels)
